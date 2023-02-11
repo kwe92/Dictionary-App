4e67ce313559200b5acc350dddb73473b5032c0e
Kweayon  Clark <Kweayon@gmail.com>
2023-02-11 01:33:17 -0600

    Added DropDown

--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { images } from "../../constants/images";
 import {
   AppBarContainer,
@@ -8,18 +8,52 @@ import {
   DropDownIcon,
   RightContentContainer,
   VerticalLine,
+  DropDownContent,
+  DropDownItem,
 } from "./AppBarStyles";
 import Switch from "./switch/Switch";
 
+// TODO: Tag anonymous functions / callbacks with variable names
+
 const AppBar = (props: {}) => {
+  const [fontOption, setFontOption] = useState("Sans Serif");
+  const [displayDropdown, SetdisplayDropdown] = useState("none");
+
+  const dropdownController = () => {
+    SetdisplayDropdown((prevState) => (prevState === "none" ? "auto" : "none"));
+  };
+
   return (
     <AppBarContainer>
       <Logo src={images.logo} alt="logo.svg" />
 
       <RightContentContainer>
-        <FontDropDownContainer>
-          <p style={{ color: "black" }}>Sans Serif</p>
+        <FontDropDownContainer onClick={dropdownController}>
+          <p style={{ color: "black" }}>{fontOption}</p>
           <DropDownIcon src={images.arrowDown} alt="arrow-down.svg" />
+          <DropDownContent display={displayDropdown}>
+            <DropDownItem
+              onClick={() => {
+                setFontOption("Sans Serif");
+              }}
+            >
+              Sans Serif
+            </DropDownItem>
+            <DropDownItem
+              onClick={() => {
+                setFontOption("Serif");
+              }}
+            >
+              Serif
+            </DropDownItem>
+            <DropDownItem
+              onClick={() => {
+                setFontOption("Mon");
+              }}
+            >
+              Mon
+            </DropDownItem>
+          </DropDownContent>
         </FontDropDownContainer>
 
         <VerticalLine />
