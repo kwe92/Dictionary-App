416d4d55f794d65bddb53e487f6e23b2dd99bd69
Kweayon  Clark <Kweayon@gmail.com>
2023-02-11 01:33:56 -0600

    Update

--- src/AppStyles.tsx
+++ src/AppStyles.tsx
@@ -4,7 +4,7 @@ import { Column } from "./styles/layout/Flex";
 const AppContainerWrapper = styled(Column)`
   width: 100%;
   height: 100vh;
-  background: purple;
+  background: #0fa2a9;
   align-items: center;
   justify-content: center;
 `;
@@ -12,7 +12,7 @@ const AppContainerWrapper = styled(Column)`
 const AppContainer = styled(Column)`
   width: 60%;
   height: 100%;
-  background: orange;
+  background: #7d3865;
 `;
 
 export { AppContainer, AppContainerWrapper };
