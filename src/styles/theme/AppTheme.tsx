82a0d394a363e3d0af5e6a5c8bd94029d7fb32de
Kweayon  Clark <Kweayon@gmail.com>
2023-02-10 21:45:04 -0600

    App Theme

---
+++ src/styles/theme/AppTheme.tsx
@@ -0,0 +1,20 @@
+const AppTheme: ThemeInterface = {
+  darkColors: {
+    shade0: "rgba(5,5,5,1)",
+    shade1: "rgba(31,31,31,1)",
+    shade2: "rgba(45,45,45,1)",
+    shade3: "rgba(58,58,58,1)",
+  },
+  lightColors: {
+    shade0: "rgba(131,131,131,1)",
+    shade1: "rgba(233,233,233,1)",
+    shade2: "rgba(242,242,242,1)",
+    shade3: "rgba(255,255,255,1)",
+  },
+  otherColors: {
+    purpleColor: "rgba(164,69,237,1)",
+    redColor: "rgba(255,82,82,1)",
+  },
+};
+
+export default AppTheme;
