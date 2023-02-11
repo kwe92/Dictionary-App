7ac3fc48502ae147982d982566a4ac3ffb7c2ad4
Kweayon  Clark <Kweayon@gmail.com>
2023-02-10 22:56:50 -0600

    Added AppBar Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import AppTheme from "./styles/theme/AppTheme";
 import { AppContainer, AppContainerWrapper } from "./AppStyles";
-
+import AppBar from "./components/appBar/AppBar";
 const theme = AppTheme;
 
 const App = (props: {}) => {
@@ -10,6 +10,7 @@ const App = (props: {}) => {
     <ThemeProvider theme={theme}>
       <AppContainerWrapper>
         <AppContainer>
+          <AppBar />
           <h1>App</h1>;
         </AppContainer>
       </AppContainerWrapper>
