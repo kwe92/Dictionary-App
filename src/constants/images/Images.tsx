3c5f008458d99598812e0b36504e189aab0b3865
Kweayon  Clark <Kweayon@gmail.com>
2023-02-10 22:56:20 -0600

    Image Object

---
+++ src/constants/images/Images.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import arrowDown from "../../assets/images/icon-arrow-down.svg";
+import moon from "../../assets/images/icon-moon.svg";
+import newWindow from "../../assets/images/icon-new-window.svg";
+import play from "../../assets/images/icon-play.svg";
+import search from "../../assets/images/icon-search.svg";
+import logo from "../../assets/images/logo.svg";
+
+export default {
+  arrowDown,
+  moon,
+  newWindow,
+  play,
+  search,
+  logo,
+};
