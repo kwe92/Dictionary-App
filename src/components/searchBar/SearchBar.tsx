e673ba3d63aa7808c8873739de97d0ea971b8df9
Kweayon  Clark <Kweayon@gmail.com>
2023-02-11 19:36:47 -0600

    Added Fetch

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import useFetch from "../../constants/hooks/useFetch";
 import { images } from "../../constants/images";
+
 import {
   SearchBarContainer,
   SearchIcon,
@@ -10,35 +12,45 @@ import {
   ClearIcon,
 } from "./SearchBarStyles";
 
-const SearchBar = (props: {}) => {
-  const [userInput, setUserInput] = useState("");
+// TODO Need to figure out how to mat the HiX appear when the user types now
 
-  const handleUserInput = (event: any) => {
-    const input = event.target.value;
-    console.log("USer Input: ", input);
-    setUserInput(input);
-  };
+const SearchBar = (props: {}) => {
+  const [clear, setClear] = useState(false);
+  const { word, setWord } = useFetch();
+
+  const wordRef: RefObj = useRef();
 
   const handleSubmit = (event: any) => {
+    const refResult = wordRef.current!.value;
     event.preventDefault();
-    setUserInput("");
+    console.log("Submitted Word: ");
+    console.log("Word Ref: ", refResult);
+    wordRef.current!.value = "";
+    setWord(refResult);
+    setClear(false);
   };
 
   const handleClear = () => {
-    setUserInput("");
+    wordRef.current!.value = "";
+    setClear(false);
   };
 
   return (
     <SearchBarContainer>
-      <StyledForm onSubmit={handleSubmit}>
+      <StyledForm
+        onSubmit={handleSubmit}
+        onClick={() => {
+          setClear(true);
+        }}
+      >
         <StyledInput
           placeholder="Search for any word..."
-          value={userInput}
-          onChange={handleUserInput}
+          // value={userInput}
+          ref={wordRef as unknown as any}
         />
       </StyledForm>
 
-      {userInput && (
+      {clear && (
         <ClearIconContainer onClick={handleClear}>
           <ClearIcon />
         </ClearIconContainer>
