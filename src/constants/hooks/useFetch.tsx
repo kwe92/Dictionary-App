a3b7d361628b5df4398fbb0b05f6f9c68dd88828
Kweayon  Clark <Kweayon@gmail.com>
2023-02-11 19:37:16 -0600

    useFtech Custom Hook

---
+++ src/constants/hooks/useFetch.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = () => {
+  const [word, setWord] = useState("");
+  const endpint = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.REACT_APP_API_KEY}`;
+
+  useEffect(() => {
+    axios.get(endpint).then((result) => {
+      console.log("API Call Result", result);
+    });
+  });
+  return { word, setWord };
+};
+
+export default useFetch;
