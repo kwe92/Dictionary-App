f8a57917cac26906b1101a83bee94d3ebc9c8384
Kweayon  Clark <Kweayon@gmail.com>
2023-02-13 22:13:33 -0600

    Working on Parsing API Call

--- src/constants/hooks/useFetch.tsx
+++ src/constants/hooks/useFetch.tsx
@@ -1,15 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = () => {
-  const [word, setWord] = useState("Abyss");
-  const endpint = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.REACT_APP_API_KEY}`;
+// TODO: Need a better way to parse object results
+
+const useFetch = (userInput: string) => {
+  const [word, setWord] = useState([]);
+  const endpint = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${userInput}?key=${process.env.REACT_APP_API_KEY}`;
+
+  const fetchWord = async (userInput: string) => {
+    const data = await axios.get(endpint);
+
+    console.log("Data: ", data.data[0]["hwi"]["hw"]);
+    console.log("Data: ", data.data);
+
+    // const result = data.data;
+    if (
+      data.data[0]["hwi"]["hw"] === userInput.toLocaleLowerCase() ||
+      (data.data[0]["meta"]["id"] === userInput.toLocaleLowerCase() &&
+        data.data[0]["fl"] === "noun")
+    ) {
+      const wordObject = {
+        word: userInput.toLowerCase(),
+        pronunciation: data.data[0]["hwi"]["prs"][0]["mw"],
+        partOfSpeach: data.data[0]["fl"],
+        definition: data.data[0]["shortdef"],
+      };
+      setWord(wordObject as any);
+    }
+  };
 
   useEffect(() => {
-    axios.get(endpint).then((result) => {
-      console.log("API Call Result", result);
-    });
-  });
+    fetchWord(userInput);
+  }, [userInput]);
   return { word, setWord };
 };
 
