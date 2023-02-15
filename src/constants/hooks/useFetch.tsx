e048320aaebc766ab9929b45bcc85c8fc35275fc
Kweayon  Clark <Kweayon@gmail.com>
2023-02-14 22:31:19 -0600

    Added Part of Speech

--- src/constants/hooks/useFetch.tsx
+++ src/constants/hooks/useFetch.tsx
@@ -5,34 +5,65 @@ import axios from "axios";
 
 const useFetch = (userInput: string) => {
   const [word, setWord] = useState([]);
+  const partsOfSpeech = [
+    "noun",
+    "pronoun",
+    "verb",
+    "adjective",
+    "adverb",
+    "preposition",
+    "conjunction",
+    "interjection",
+  ];
   const endpint = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${userInput}?key=${process.env.REACT_APP_API_KEY}`;
+  let wordPOS: any = [];
 
   const fetchWord = async (userInput: string) => {
     const data = await axios.get(endpint);
 
     console.log("Data: ", data.data[0]["hwi"]["hw"]);
-    console.log("Data: ", data.data);
+    console.log("Data All: ", data.data);
 
     // const result = data.data;
-    if (
-      data.data[0]["hwi"]["hw"] === userInput.toLocaleLowerCase() ||
-      (data.data[0]["meta"]["id"] === userInput.toLocaleLowerCase() &&
-        data.data[0]["fl"] === "noun")
-    ) {
-      const wordObject = {
-        word: userInput.toLowerCase(),
-        pronunciation: data.data[0]["hwi"]["prs"][0]["mw"],
-        partOfSpeach: data.data[0]["fl"],
-        definition: data.data[0]["shortdef"],
-      };
-      setWord(wordObject as any);
+
+    for (let i = 0; i < partsOfSpeech.length; i++) {
+      if (
+        data.data[0]["hwi"]["hw"].replace("*", "") ===
+          userInput.toLocaleLowerCase() ||
+        (data.data[0]["meta"]["id"] === userInput.toLocaleLowerCase() &&
+          data.data[0]["fl"] === partsOfSpeech[i])
+      ) {
+        const wordObject = {
+          word: userInput.toLowerCase(),
+          pronunciation: data.data[i]["hwi"]["prs"][i]["mw"],
+          partOfSpeach: data.data[i]["fl"],
+          definition: data.data[i]["shortdef"],
+        };
+
+        wordPOS.push(wordObject);
+      }
     }
+    // setWord(wordObject as any);
+
+    // if (
+    //   data.data[0]["hwi"]["hw"] === userInput.toLocaleLowerCase() ||
+    //   (data.data[0]["meta"]["id"] === userInput.toLocaleLowerCase() &&
+    //     data.data[0]["fl"] === "noun")
+    // ) {
+    //   const wordObject = {
+    //     word: userInput.toLowerCase(),
+    //     pronunciation: data.data[0]["hwi"]["prs"][0]["mw"],
+    //     partOfSpeach: data.data[0]["fl"],
+    //     definition: data.data[0]["shortdef"],
+    //   };
+    //   setWord(wordObject as any);
+    // }
   };
 
   useEffect(() => {
     fetchWord(userInput);
   }, [userInput]);
-  return { word, setWord };
+  return { wordPOS, setWord };
 };
 
 export default useFetch;
