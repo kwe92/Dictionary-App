4048337d63562fce34bd1048ab576c0d17722d09
Kweayon  Clark <Kweayon@gmail.com>
2023-02-16 12:14:43 -0600

    Added Definitions

--- src/components/definition/DefinitionSection.tsx
+++ src/components/definition/DefinitionSection.tsx
@@ -1,31 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { images } from "../../constants/images";
 
 import {
+  DefinitionListTile,
+  DefinitionListTileContainer,
+  DefinitionTittle,
+  // Dot,
+  HorizonalLine,
   ListTile,
   ListTileBottom,
   ListTileContentContainer,
   ListTileTitle,
   MainContainer,
   PlayButton,
+  POSContainer,
 } from "./DefinitionSectionStyles";
 
-const DefinitionSection = (props: { wordObj: any }) => {
-  console.log("FROM DEFINITION SECTION: ", props.wordObj);
+const DefinitionSection = (props: { wordObj: Array<WordInterface> }) => {
+  const definitionList =
+    props.wordObj.length > 0
+      ? props.wordObj[0].definition.map((definition, index) => (
+          <li key={index * 3.14}>{definition}</li>
+        ))
+      : [""];
   return (
     <MainContainer>
       <ListTile>
         <ListTileContentContainer>
-          <ListTileTitle>{props.wordObj["word"]}</ListTileTitle>
+          <ListTileTitle>
+            {props.wordObj.length > 0 && props.wordObj[0]["word"]}
+          </ListTileTitle>
           <ListTileBottom>
-            {props.wordObj["pronunciation"]
-              ? `/${props.wordObj["pronunciation"]}/`
+            {props.wordObj[0] && props.wordObj[0]["pronunciation"]
+              ? `/${props.wordObj[0]["pronunciation"]}/`
               : ""}
           </ListTileBottom>
         </ListTileContentContainer>
 
         <PlayButton src={images.play} />
       </ListTile>
+      <POSContainer>
+        <p>{props.wordObj.length > 0 && props.wordObj[0]["partOfSpeach"]}</p>
+        <HorizonalLine />
+      </POSContainer>
+      <DefinitionTittle>Meaning</DefinitionTittle>
+      <DefinitionListTileContainer>
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            gap: "1rem",
+          }}
+        >
+          {definitionList}
+        </ul>
+      </DefinitionListTileContainer>
     </MainContainer>
   );
 };
