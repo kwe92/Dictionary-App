d60c4fb888b39ca021cf0a4e224c6c7041715222
Kweayon  Clark <Kweayon@gmail.com>
2023-02-16 12:16:53 -0600

    Word Model

---
+++ src/constants/models/WordModel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const wordModel = (props: WordInterface): WordInterface => {
+  return {
+    word: props.word,
+    pronunciation: props.pronunciation,
+    partOfSpeach: props.partOfSpeach,
+    definition: props.definition,
+  };
+};
