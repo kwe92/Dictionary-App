b83291e5eef492aa588fb1a5a31a380f1b8a26b6
Kweayon  Clark <Kweayon@gmail.com>
2023-02-20 15:27:46 -0600

    Updated Styles

--- src/components/appBar/switch/SwitchStyles.tsx
+++ src/components/appBar/switch/SwitchStyles.tsx
@@ -3,7 +3,7 @@ import AppTheme from "../../../styles/theme/AppTheme";
 import SwitchComponent from "@mui/material/Switch";
 
 const themes = AppTheme;
-const StyledSwitch = styled(SwitchComponent)(({ theme }) => ({
+const _StyledSwitch = styled(SwitchComponent)(({ theme }) => ({
   "& .MuiSwitch-switchBase.Mui-checked": {
     color: "white",
   },
@@ -11,5 +11,11 @@ const StyledSwitch = styled(SwitchComponent)(({ theme }) => ({
     backgroundColor: themes.otherColors.purpleColor,
   },
 }));
+
+const StyledSwitch = styled(_StyledSwitch)`
+  @media screen and (max-width: 450px) {
+    margin-right: 0.4375rem;
+  }
+`;
 
 export default StyledSwitch;
