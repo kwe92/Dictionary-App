4f7d5f72818d460659d9238453fee3f017618340
Kweayon  Clark <Kweayon@gmail.com>
2023-02-28 12:29:39 -0600

    Refactor

--- src/components/errorPage/ErrorPage.tsx
+++ src/components/errorPage/ErrorPage.tsx
@@ -7,15 +7,19 @@ import {
   MainContainer,
 } from "./ErrorPageStyles";
 
-export const ErrorPage = (props: { unknownWord: string }) => {
+export const ErrorPage = (props: {
+  unknownWord: string;
+  setUserInput: Function;
+  otherWords: string[];
+}) => {
   return (
     <MainContainer>
       <IconSadFace src={images.sadFace} alt="sadface.png" />
       <ErrorTittle>{`No Definitions Found For: ${props.unknownWord}`}</ErrorTittle>
-      <ErrorBody>{_errorMSG}</ErrorBody>
+      <ErrorBody>{errorMSG}</ErrorBody>
     </MainContainer>
   );
 };
 
-const _errorMSG =
+const errorMSG =
   "Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.";
