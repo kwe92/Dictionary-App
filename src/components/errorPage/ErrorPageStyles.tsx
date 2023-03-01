7a0d593d00ecc0fd5d8aaa19b1af32a147607fdb
Kweayon  Clark <Kweayon@gmail.com>
2023-02-28 18:48:46 -0600

    Added Neumorphic Styling

--- src/components/errorPage/ErrorPageStyles.tsx
+++ src/components/errorPage/ErrorPageStyles.tsx
@@ -4,13 +4,15 @@ import { Column } from "../../styles/layout/Flex";
 const MainContainer = styled(Column)`
   align-items: center;
   justify-content: space-evenly;
+  text-align: center;
   width: 100%;
-  height: 26rem;
-  //   background: lightgreen;
   margin-top: 5rem;
+  gap: 1.75rem;
+  padding-bottom: 3.75rem;
 
   @media screen and (max-width: 450px) {
-    margin-top: 1.5rem;
+    margin-top: 2.125rem;
+    padding-bottom: 2.75rem;
   }
 `;
 
@@ -31,6 +33,57 @@ const ErrorBody = styled.p`
   color: ${({ theme }: { theme: ThemeInterface }) => theme.lightColors.shade0};
 `;
 
+const StyledLI = styled.li`
+  padding: 1rem 2rem;
+
+  border-radius: 1rem;
+  cursor: pointer;
+  font-size: 1.125rem;
+  text-align: center;
+
+  @media screen and (min-width: 1024px) {
+    padding: 1.125rem 2.3125rem;
+  }
+
+  @media screen and (min-width: 1300x) {
+  }
+`;
+
+const StyledUL = styled.ul`
+  display: grid;
+
+  grid-template-columns: auto auto;
+
+  grid-row-gap: 1.75rem;
+
+  list-style-type: none;
+
+  row-gap: 1.3125rem;
+
+  column-gap: 1.5rem;
+
+  @media screen and (min-width: 750px) {
+    grid-template-columns: auto auto auto;
+    column-gap: 1.5rem;
+  }
+
+  // @media screen and (min-width: 1200px) {
+  //   grid-template-columns: auto auto auto auto auto auto auto;
+  //   row-gap: 1.75rem;
+  // }
+
+  @media screen and (min-width: 1300x) {
+    grid-template-columns: auto auto auto auto auto auto auto auto auto;
+  }
+`;
+
 // const IconImageContainer = styled.div``;
 
-export { MainContainer, IconSadFace, ErrorBody, ErrorTittle };
+export {
+  MainContainer,
+  IconSadFace,
+  ErrorBody,
+  ErrorTittle,
+  StyledLI,
+  StyledUL,
+};
